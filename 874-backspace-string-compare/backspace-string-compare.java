95153f835b42339ad5d09c4d3712ce3248d20563
Sumit Singh <sumit.singha9711@gmail.com>
2026-07-23 23:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (52.80%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skips = 0;
+        int skipt = 0;
+
+        while(i >= 0 || j >= 0){
+            
+            while(i >= 0){
+                if(s.charAt(i) == '#'){
+                    skips++;
+                    i--;
+                }
+                else if (skips > 0){
+                    skips--;
+                    i--;
+                }
+                else 
+                     break;
+            }
+
+            while(j >= 0){
+                if(t.charAt(j) == '#'){
+                    skipt++;
+                    j--;
+                }
+                else if (skipt > 0){
+                    skipt--;
+                    j--;
+                }
+                else 
+                     break;
+            }
+
+            if(i >= 0 && j >= 0){
+                if(s.charAt(i) != t.charAt(j))
+                    return false;
+            }
+            else if (i >= 0 || j >= 0)
+                return false;
+
+            i--;
+            j--;
+
+        }
+
+        return true;
+
+    }
+}
